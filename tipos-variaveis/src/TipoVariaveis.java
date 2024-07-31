70611ead12a68146dddd551a8e0917956bb28f12
marco <mabdsouza@outlook.com>
2024-07-31 09:40:51 -0300

    tipos variaveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -3,3 +3,46 @@ public class TipoVariaveis {
         System.out.println("Hello, World!");
     }
 }
+
+
+// int idade;
+/*
+    int anoF = 2021;
+    double sal = 2.50;
+    float pi = 3.14F;
+    long cpf = 938471348L
+    byte idade = 123;
+*/
+/*
+    = igualdade
+    + adição
+    - subtração
+    * multiplicação
+    / divisão
+    ++ incremento
+    -- decremento
+    ! negação
+*/
+/*
+    == igualdade entre elementos
+    equals Objetos
+    != diferente
+    && E
+    || OU
+    
+*/
+/*
+<Condicional> ? <Caso true> : <Caso false>
+*/
+
+/*
+    public class MyClass {
+        public double somar(double num1, double num2) {
+            // logica - Finalidade do Método
+            return ...;
+        }
+        public void imprimit(String texto) {
+            // Logica - Finalidade do Método
+        }
+    }
+*/
