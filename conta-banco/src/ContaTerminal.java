d01871df9ff5004d3f9916bb9c93b7233172e396
marco <mabdsouza@outlook.com>
2024-07-31 17:18:45 -0300

    conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner; // Import the Scanner class
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner myObj = new Scanner(System.in)) {
+    
+            System.out.println("Digite o numero da Agencia: ");
+            String agency = myObj.nextLine();
+            
+            System.out.println("Digite o numero da Conta: ");
+            int numberAccount = myObj.nextInt();
+
+            myObj.nextLine(); // Consumir a linha vazia deixada por nextInt()
+
+            System.out.println("Digite o nome do cliente: ");
+            String nameClient = myObj.nextLine();
+
+            System.out.println("Digite o saldo da conta: ");
+            float balance = myObj.nextFloat();
+            
+
+            System.out.printf("Ola %s, obrigado por criar uma conta em nosso banco, sua agencia e %s, conta %s e seu saldo %.2f ja está disponivel para saque.%n", nameClient, agency, numberAccount, balance);
+        }
     }
 }
